407d465021bdede1fc0a4067d7bdca9b4587af6e
Paul Mwaniki Kimani <paul.kimani@strathmore.edu>
2025-12-27 13:42:43 +0300

    Remove star button prompt from Module 1
    Removed GitHub star button prompt from the introduction.

--- pages/course/2024_cohort/Module_1_Introduction_&_Prerequisites.py
+++ pages/course/2024_cohort/Module_1_Introduction_&_Prerequisites.py
@@ -19,9 +19,6 @@ if you have troubles setting up the environment and following along with the vid
 
 st.markdown("---")
 
-st.markdown("""### ⭐ If you are enjoying your learning experience please leave a Star <iframe src="https://ghbtns.com/github-btn.html?user=hamagistral&repo=de-zoomcamp-ui&type=star&count=true"  width="150" height="20" title="GitHub"></iframe>
-""", unsafe_allow_html=True)
-
 st.markdown("---")
 
 st.info("💻 Docker + Postgres [Code](https://github.com/DataTalksClub/data-engineering-zoomcamp/tree/main/01-docker-terraform/2_docker_sql)")
